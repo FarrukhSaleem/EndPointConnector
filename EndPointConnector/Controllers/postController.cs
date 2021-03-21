0c88b061f271314ba242316716409a88e3653cea
Farrukh Saleem Sheikh <farrukhsaleem_01@yahoo.com>
2021-03-21 13:40:28 +0500

    Implement GET,POST,PUT,Delete

--- EndPointConnector/Controllers/postController.cs
+++ EndPointConnector/Controllers/postController.cs
@@ -1,44 +1,90 @@
 ﻿using EndPointConnector.Context;
 using EndPointConnector.Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Http;
-using System.Web.Mvc;
+using HttpDeleteAttribute = System.Web.Http.HttpDeleteAttribute;
 using HttpGetAttribute = System.Web.Http.HttpGetAttribute;
 using HttpPostAttribute = System.Web.Http.HttpPostAttribute;
+using HttpPutAttribute = System.Web.Http.HttpPutAttribute;
 using RouteAttribute = System.Web.Http.RouteAttribute;
 
 namespace EndPointConnector.Controllers
 {
-    public class postController : ApiController
-    {
-        postContext PostContext;
-        [HttpGet]
-        [Route("api/post/GetAllPosts")]
-        public List<post> GetAllPosts()
-        {
-            PostContext = new postContext();
-            List<post> posts = new List<post>();
-            posts = PostContext.Posts.ToList();
+	public class postController : ApiController
+	{
+		postContext PostContext;
 
-            return posts;
-        }
-        [HttpPost]
-        [Route("api/post/CreatePost")]
-        public int CreatePost(post post1)
-        {
-            int id = -1;
-            PostContext = new postContext();
-            //post post1 = new post
-            //{
-            //    Title=title
-            //};
-            post newItem = PostContext.Posts.Add(post1);
-            PostContext.SaveChanges();
-            id = newItem.Id;
-            return id;
-        }
-    }
+		public postController()
+		{
+
+			PostContext = new postContext();
+		}
+
+		[HttpGet]
+		[Route("api/post/GetAllPosts")]
+		public IEnumerable<post> GetAllPosts()
+		{
+			return PostContext.Posts.ToList();
+		}
+
+		[HttpGet]
+		[Route("api/post/id")]
+		public post GetPost(int id)
+		{
+			post p = PostContext.Posts.SingleOrDefault(c => c.Id == id);
+
+			if (p == null)
+				throw new HttpResponseException(System.Net.HttpStatusCode.NotFound);
+			return p;
+		}
+
+		[HttpPost]
+		[Route("api/post/CreatePost")]
+		public post CreatePost(post post1)
+		{
+			if (!ModelState.IsValid)
+				throw new HttpResponseException(System.Net.HttpStatusCode.BadRequest);
+
+			PostContext.Posts.Add(post1);
+			PostContext.SaveChanges();
+
+			return post1;
+		}
+
+		[HttpPut]
+		[Route("api/post/UpdatePost/id")]
+		public void UpdatePost(int id, post post1)
+		{
+			if (!ModelState.IsValid)
+				throw new HttpResponseException(System.Net.HttpStatusCode.BadRequest);
+
+			post dbpost = PostContext.Posts.SingleOrDefault(p => p.Id == id);
+
+			if (dbpost == null)
+				throw new HttpResponseException(System.Net.HttpStatusCode.NotFound);
+
+			dbpost.Body = post1.Body;
+			dbpost.Title = post1.Title;
+			dbpost.UserId = post1.UserId;
+
+			PostContext.SaveChanges();
+		}
+
+		[HttpDelete]
+		[Route("api/post/DeletePost/id")]
+		public void DeletePost(int id)
+		{
+			if (!ModelState.IsValid)
+				throw new HttpResponseException(System.Net.HttpStatusCode.BadRequest);
+
+			post dbpost = PostContext.Posts.SingleOrDefault(p => p.Id == id);
+
+			if (dbpost == null)
+				throw new HttpResponseException(System.Net.HttpStatusCode.NotFound);
+
+			PostContext.Posts.Remove(dbpost);
+			PostContext.SaveChanges();
+		}
+	}
 }
